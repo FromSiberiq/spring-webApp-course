6eb9852bd5e36e5c21b589f988afda1430f4cbb2
Okami <mr.naydyuk2@gmail.com>
2025-03-12 15:26:18 +0800

    Lesson 4 - Inversion of Control (IoC)

---
+++ src/main/java/org/springcourse/ClassicalMusic.java
@@ -0,0 +1,9 @@
+package org.springcourse;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+
+}
---
+++ src/main/java/org/springcourse/RapMusic.java
@@ -0,0 +1,9 @@
+package org.springcourse;
+
+public class RapMusic implements Music {
+
+    @Override
+    public String getSong() {
+        return "Eminem - Godzilla";
+    }
+}
