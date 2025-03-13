0b72d32707704e394b5bdac32b4dea06a6dbcb73
Okami <mr.naydyuk2@gmail.com>
2025-03-13 15:34:59 +0800

    lesson 8 - bean scope (область видимости)

--- src/main/java/org/springcourse/TestSpring.java
+++ src/main/java/org/springcourse/TestSpring.java
@@ -11,8 +11,24 @@ public class TestSpring {
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparing = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(comparing);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
