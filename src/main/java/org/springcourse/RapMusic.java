e09fa762433586c22fa42b94c086666bde073e2b
Okami <mr.naydyuk2@gmail.com>
2025-03-14 14:43:12 +0800

    lesson 8 - Жизненный цикл бина (Bean Lifecycle). Init, Destroy и Factory методы.

--- src/main/java/org/springcourse/RapMusic.java
+++ src/main/java/org/springcourse/RapMusic.java
@@ -2,6 +2,16 @@ package org.springcourse;
 
 public class RapMusic implements Music {
 
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
+
+
+
     @Override
     public String getSong() {
         return "Eminem - Godzilla";
