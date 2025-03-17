16642fb81b5ee0c1b191a45cf2afde7bb4b3d231
Okami <mr.naydyuk2@gmail.com>
2025-03-17 12:28:33 +0800

    lesson 10 - Аннотация @Autowired. Внедрение зависимостей (Dependency Injection).

--- src/main/java/org/springcourse/TestSpring.java
+++ src/main/java/org/springcourse/TestSpring.java
@@ -8,10 +8,16 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("rapMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        Music music = context.getBean("rapMusic", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//
+//        musicPlayer.playMusic();
 
-        musicPlayer.playMusic();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
